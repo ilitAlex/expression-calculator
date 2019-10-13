e90b7a5f3e470ee5e3ba2acdb4c74967d0fb5e9c
Alexandra Zaitsava <ilit_alex@tut.by>
2019-10-13 13:26:41 +0300

    init: init

--- src/index.js
+++ src/index.js
@@ -4,7 +4,25 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+let arr=expr.match(/[-*()+/]|\d+/g);
+let result;
+for (let i=0;i<arr.length;i++)
+{
+    if (arr[i]==='+')
+    {result=(+arr[i-1])+(+arr[i+1])}
+    else if (arr[i]==='*')
+    {result=(+arr[i-1])*(+arr[i+1])}
+    else if (arr[i]==='/')
+    { if (arr[i+1]==='0') {throw new Error('TypeError: Division by zero.')}
+        else result=(+arr[i-1])/(+arr[i+1])}
+    else if (arr[i]==='-')
+    {result=(+arr[i-1])-(+arr[i+1])}
+
+}
+
+    return result;
+
+
 }
 
 module.exports = {
